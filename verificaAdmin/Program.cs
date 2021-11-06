82478f02509230178e934da9951cc90b06edce1c
Allan F Ribeiro <allanfribeiro@gmail.com>
2021-11-06 16:04:55 -0300

    Início do projeto

---
+++ verificaAdmin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace verificaAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String usuario;
+            Console.WriteLine("Informa seu usuário");
+            usuario = Console.ReadLine().ToUpper();
+
+            if (!(usuario == "ADMINISTRADOR"))
+
+            {
+                Console.WriteLine("Atenção, este usuário não tem poderes administrativos e algumas tarefas serão bloqueadas");
+            }
+            else
+            {
+                Console.WriteLine("Seja bem vindo Administrador");
+            }
+        }
+    }
+}
